6d7d3211a1545187db09a9eb25fd6b1ac98b32dc
Andrew Gross <grossmail1@gmail.com>
2017-03-17 21:33:45 -0600

    first commit

---
+++ src/Components/Login.js
@@ -0,0 +1,16 @@
+import React, { PureComponent, PropTypes } from 'react'
+
+class Login extends PureComponent {
+	static propsTypes = {}
+
+	render() {
+		return (
+			<div>
+				<h2>Home</h2>
+				<p>and more stuff</p>
+			</div>
+		)
+	}
+}
+
+export default Login
