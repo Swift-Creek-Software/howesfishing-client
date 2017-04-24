70a9d3b1c7c70322ad12471a7c698039a8b84d59
Andrew Gross <grossmail1@gmail.com>
2017-04-24 08:33:11 -0600

    adding dev url

--- src/axiosClients/howesFishing.js
+++ src/axiosClients/howesFishing.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const howesFishingClient = axios.create({ //all axios can be used, shown in axios documentation
-	baseURL:'https://howesfishing-server-crobinson42.c9users.io/',
+	baseURL:'http://45.55.2.165',
 	responseType: 'json',
 	headers: {'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4Y2RhMmU2NGExZDY0NjY0ZDM5ZDA1MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTQ4OTg3MTU5MCwiZXhwIjoxNDk3MDcxNTkwfQ.WsO1amERJklG_fjakG0f4zMVQYRjr_Mpufw4a3PJBTE"}
 });
