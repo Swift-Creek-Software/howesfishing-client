667d2165590de8507b692e82211ddbcf630d1201
Andrew Gross <grossmail1@gmail.com>
2017-04-29 12:30:27 -0600

    adding error on login page

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -1,5 +1,8 @@
 import React, { PropTypes, PureComponent } from 'react'
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, SubmissionError } from 'redux-form';
+import {connect} from 'react-redux'
+
+import { login } from '../actions/UserActions'
 
 import TextField from './Common/TextField'
 import FormHeader from './Common/FormHeader'
@@ -9,12 +12,17 @@ class LoginForm extends PureComponent {
 	static propTypes = {
 		onSubmit: PropTypes.func.isRequired
 	}
+	onFormSubmit = (values) => {
+		return this.props.login(values.email, values.password).catch(error => {
+			throw new SubmissionError({_error: 'username/password are incorrect'})
+		})
+	}
 
 	render() {
-		const { handleSubmit } = this.props;
+		const { handleSubmit, error } = this.props
 
 		return (
-			<form className="panel panel-primary" onSubmit={handleSubmit}>
+			<form className="panel panel-primary" onSubmit={handleSubmit(this.onFormSubmit)}>
 				<FormHeader>Login</FormHeader>
 				<div className="panel-body">
 					<Field name="email"
@@ -29,6 +37,9 @@ class LoginForm extends PureComponent {
 						   placeholder="Enter Password"
 						   type="password"
 					/>
+					{error &&
+						<p style={{color: 'red'}}>{error}</p>
+					}
 					<button type="submit" className="btn btn-primary">Login</button>
 				</div>
 			</form>
@@ -39,6 +50,10 @@ class LoginForm extends PureComponent {
 
 LoginForm = reduxForm({
 	form: 'login' // a unique name for this form
-})(LoginForm);
+})(LoginForm)
+
+LoginForm = connect(null, {
+	login
+})(LoginForm)
 
 export default LoginForm
