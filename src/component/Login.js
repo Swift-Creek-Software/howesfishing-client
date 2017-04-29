0e87e981136c856fefc1a8d585cce74f077b9b62
Andrew Gross <grossmail1@gmail.com>
2017-04-29 12:30:18 -0600

    adding error on login page

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,7 +2,6 @@ import React, { PureComponent, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { Redirect, withRouter } from 'react-router-dom'
 
-import { login } from '../actions/UserActions'
 
 import LoginForm from './LoginForm'
 import './Common/Common.css'
@@ -10,10 +9,6 @@ import './Common/Common.css'
 class Login extends PureComponent {
 	static propTypes = {
 		user: PropTypes.object
-	}
-
-	onFormSubmit = (values) => {
-		this.props.login(values.email, values.password)
 	}
 
 	render() {
@@ -34,7 +29,5 @@ export default connect(state => {
 		return {
 			user: state.user
 		}
-	}, {
-		login
 	}
 )(withRouter(Login))
