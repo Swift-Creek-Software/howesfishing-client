909f59da2a6dc158ecf2c1370dabebe2b7297f91
Andrew Gross <grossmail1@gmail.com>
2017-07-20 15:53:25 -0600

    adding twilight available guides

--- src/component/AvailableGuides.js
+++ src/component/AvailableGuides.js
@@ -9,8 +9,8 @@ class AvailableGuides extends PureComponent {
 	static propsTypes = {}
 
 	renderAfternoonGuides = (date) => {
-		const start = date.hours(12).minutes(59).seconds(0).toDate()
-		const end = date.hours(18).minutes(1).seconds(0).toDate()
+		const start = date.hours(13).minutes(0).seconds(0).toDate()
+		const end = date.hours(18).minutes(0).seconds(0).toDate()
 		const guides = this.getAvailableGuides(start, end)
 		console.log('available guides', guides)
 		return guides.map(guide => {
@@ -19,8 +19,18 @@ class AvailableGuides extends PureComponent {
 	}
 
 	renderMorningGuides = (date) => {
-		const start = date.hours(6).minutes(59).seconds(0).toDate()
-		const end = date.hours(12).minutes(1).seconds(0).toDate()
+		const start = date.hours(7).minutes(0).seconds(0).toDate()
+		const end = date.hours(13).minutes(0).seconds(0).toDate()
+		const guides = this.getAvailableGuides(start, end)
+		console.log('available guides', guides)
+		return guides.map(guide => {
+			return <li key={guide.name}>{guide.name}</li>
+		})
+	}
+
+	renderTwilight = (date) => {
+		const start = date.hours(18).minutes(30).seconds(0).toDate()
+		const end = date.hours(20).minutes(30).seconds(0).toDate()
 		const guides = this.getAvailableGuides(start, end)
 		console.log('available guides', guides)
 		return guides.map(guide => {
@@ -46,7 +56,7 @@ class AvailableGuides extends PureComponent {
 			return (
 				<div>
 					<h2>Available Guides</h2>
-					<div style={{display: 'flex', justifyContent: 'space-around'}}>
+					<div style={{display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap'}}>
 						<div>
 							<h3>Morning</h3>
 							<ul>
@@ -57,6 +67,12 @@ class AvailableGuides extends PureComponent {
 							<h3>Afternoon</h3>
 							<ul>
 								{this.renderAfternoonGuides(date)}
+							</ul>
+						</div>
+						<div>
+							<h3>Twilight</h3>
+							<ul>
+								{this.renderTwilight(date)}
 							</ul>
 						</div>
 					</div>
